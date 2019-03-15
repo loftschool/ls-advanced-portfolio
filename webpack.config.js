02ecd7adedbc448e0f05c9cb57801c62d4ae2f9b
webbee1 <hell.web@yandex.ru>
2019-03-15 17:26:33 +0200

    Public path was changed to ./

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/';
+  const publicPath = './';
 
   const pcss = {
     test: /\.(p|post|)css$/,
