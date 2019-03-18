fd8990eddc8d8ebedbe711464205e4c1c0b483c6
Nikita <nrz2000@mail.ru>
2019-03-18 23:29:09 +0300

    Add full project(sad)

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/';
+  const publicPath = '/nrz2000.github.io';
 
   const pcss = {
     test: /\.(p|post|)css$/,
