b3483a3207adf5934a4071c753410806c2d826a6
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-07-08 13:08:27 +0300

    исправил путь для inline-svg

--- postcss.config.js
+++ postcss.config.js
@@ -2,30 +2,30 @@ const fs = require("fs");
 
 module.exports = {
   syntax: "postcss-scss",
-  parser: 'postcss-scss',
+  parser: "postcss-scss",
   plugins: [
     require("postcss-easy-import")({
-      extensions: ".pcss"
+      extensions: ".pcss",
     }),
     require("autoprefixer")({
-      cascade: false
+      cascade: false,
     }),
     require("postcss-advanced-variables")({
       variables: JSON.parse(
         fs.readFileSync("./src/styles/variables.json", "utf-8")
-      )
+      ),
     }),
     require("postcss-nested"),
     require("postcss-rgb"),
     require("postcss-inline-svg")({
       removeFill: true,
-      path: "./src/images/icons"
+      paths: ["./src/images/icons"],
     }),
     require("cssnano"),
     require("postcss-pxtorem")({
       rootValue: 16,
       propList: ["*", "!*border*"],
-      selectorBlackList: [/^html$/]
-    })
-  ]
+      selectorBlackList: [/^html$/],
+    }),
+  ],
 };
