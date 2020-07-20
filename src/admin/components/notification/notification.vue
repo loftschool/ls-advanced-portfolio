321e6edd045ba7e334d8c180300799cba63dfd5b
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-07-20 17:33:34 +0300

    notification stories

---
+++ src/admin/components/notification/notification.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="inner-tooltip-container" :class="type">
+    <div class="tooltip__text">{{text}}</div>
+    <button v-on="$listeners" class="tooltip__close"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "success"
+    },
+    text: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="postcss" scoped src="./notification.pcss"></style>
