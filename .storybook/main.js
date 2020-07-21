15b108dc6a8e2a5663a6d753704f8471be00728a
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-07-21 17:47:02 +0300

    добавил темную тему

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,18 +2,17 @@ const custom = require("../webpack.config.js")(null, "development");
 
 module.exports = {
   stories: ["../src/admin/**/*.stories.js"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-backgrounds/register",
+    "storybook-dark-mode/register"
+  ],
   webpackFinal: (config) => {
-    // config.module.rules.push({
-    //   test: /\.(p|post|)css$/,
-    //   use: ["vue-style-loader", "css-loader", "postcss-loader"],
-    // });
-
-    // return config;
     return {
       ...config,
       module: { ...config.module, rules: custom.module.rules },
-      resolve: custom.resolve
+      resolve: custom.resolve,
     };
   },
 };
--- src/admin/components/tag/tag.stories.js
+++ src/admin/components/tag/tag.stories.js
@@ -14,4 +14,9 @@ export const defaultView = () => ({
 
 defaultView.story = {
   name: "Стандартный вид",
+  parameters: {
+    backgrounds: [
+      { name: 'grey', value: '#8395a7', default: true },
+    ],
+  },
 };
