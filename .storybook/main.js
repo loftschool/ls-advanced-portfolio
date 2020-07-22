9ba32d0ff7d7d1887f862148104437e5d22b5343
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-07-22 10:37:15 +0300

    плагин storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const custom = require("../webpack.config.js")(null, "development");
+const path = require("path");
 
 module.exports = {
   stories: ["../src/admin/**/*.stories.js"],
@@ -6,12 +7,40 @@ module.exports = {
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-backgrounds/register",
-    "storybook-dark-mode/register"
+    "storybook-dark-mode/register",
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        rule: {
+          test: [/\.stories\.jsx?$/],
+          include: [path.resolve(__dirname, "../src/admin/components")],
+        },
+      },
+    },
   ],
   webpackFinal: (config) => {
+    const rules = [
+      ...custom.module.rules,
+      {
+        test: /\.stories\.jsx?$/,
+        loaders: [
+          {
+            loader: require.resolve("@storybook/source-loader"),
+            options: {
+              prettierConfig: {
+                printWidth: 100,
+                singleQuote: false,
+              },
+            },
+          },
+        ],
+        enforce: "pre",
+      },
+    ];
+
     return {
       ...config,
-      module: { ...config.module, rules: custom.module.rules },
+      module: { ...config.module, rules },
       resolve: custom.resolve,
     };
   },
